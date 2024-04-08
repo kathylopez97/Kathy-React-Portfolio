5a272ef788135e2cf14153a35fca2755c3afad0b
kathylopez97 <146994593+kathylopez97@users.noreply.github.com>
2024-04-08 14:03:26 -0700

    adding starter code from react

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// import react and vite
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import localhost 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
+})
