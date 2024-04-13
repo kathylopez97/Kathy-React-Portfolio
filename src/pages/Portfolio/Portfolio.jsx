6045de53eb86b651f6b6854b3d855d16f2a4ff62
kathylopez97 <146994593+kathylopez97@users.noreply.github.com>
2024-04-12 20:12:24 -0700

    adding description to portfolio

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -24,36 +24,47 @@ const Portfolio = () => {
         
            
            <a href="https://kathylopez97.github.io/code-quiz/">
-                <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img1} alt="codequiz" />
+             <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img1} alt="codequiz" />
+                    <p className = 'text-white'> Code Quiz</p>      
+                    <p className ='text-white'>HTML, CSS and JAVASCRIPT</p>     
             </a>
            
 
-             <a href="https://kathylopez97.github.io/code-quiz/">
-                
-             <img data-aos="fade-down" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img2} alt="workday scheduler" />
+             <a href="https://kathylopez97.github.io/work-day-scheduler/">
+                   <img data-aos="fade-down" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img2} alt="workday scheduler" />
+                   <p className = 'text-white'>Workday scheduler</p>  
+                   <p className ='text-white'>HTML, CSS and JAVASCRIPT</p> 
             </a>
             
-            <a href="https://kathylopez97.github.io/code-quiz/">
-                
-                <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img3} alt="workday scheduler" />
+            <a href="https://kathylopez97.github.io/javascript-homework/">
+                  <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img3} alt="workday scheduler" />
+                  <p className = 'text-white'>Password Generator</p>  
+                   <p className ='text-white'>HTML, CSS and JAVASCRIPT</p> 
+              
                </a>
                
    
-               <a href="https://kathylopez97.github.io/code-quiz/">
+               <a href="https://www.youtube.com/watch?v=oSJwXXfOJ9c">
                 
                 <img data-aos="fade-down" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img4} alt="workday scheduler" />
+                <p className = 'text-white'>Employee Tracker</p>  
+                <p className ='text-white'> CMS, MYSQL ,AND NODE.JS</p> 
                </a>
                
 
-               <a href="https://kathylopez97.github.io/code-quiz/">
+               <a href="https://ihateudvrk.github.io/FirstProject/">
                 
                 <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img5} alt="workday scheduler" />
+                <p className = 'text-white'>Music Junky</p>  
+                <p className ='text-white'> HTML , CSS AND JAVASCRIPT</p> 
                </a>
                
    
-               <a href="https://kathylopez97.github.io/code-quiz/">
+               <a href="https://convention-planner-f58cd02bb10c.herokuapp.com/">
                 
                 <img data-aos="fade-down" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-text-sky-500 b_glow" src={img6} alt="workday scheduler" />
+                <p className = 'text-white'>Convention Planner</p>  
+                <p className ='text-white'> HTML , CSS, AND NODE JS</p> 
                </a>
                
 
